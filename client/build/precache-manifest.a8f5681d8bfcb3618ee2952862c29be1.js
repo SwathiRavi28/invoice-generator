98e12f407e0c851f51c14dcabeaa8987beb71140
SwathiRavi28 <68095540+SwathiRavi28@users.noreply.github.com>
2021-04-22 19:00:42 +0530

    Add files via upload

---
+++ client/build/precache-manifest.a8f5681d8bfcb3618ee2952862c29be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f510eb41d95a6a7b4e2b",
+    "url": "/static/css/main.44dcb835.chunk.css"
+  },
+  {
+    "revision": "f510eb41d95a6a7b4e2b",
+    "url": "/static/js/main.f510eb41.chunk.js"
+  },
+  {
+    "revision": "934cddcb15e00c8a6f94",
+    "url": "/static/js/1.934cddcb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eeee6d9ad044e71f349aa0844448c983",
+    "url": "/index.html"
+  }
+];
